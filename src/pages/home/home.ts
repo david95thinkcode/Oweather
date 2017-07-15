30c90045a6440dd94b9c84797baed4bf8a5c1b5c
david95thinkcode <davidhihea@gmail.com>
2017-07-15 02:56:53 +0100

    Style improved

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { Geolocation }                      from '@ionic-native/geolocation';
+import { Component }                            from    '@angular/core';
+import { NavController }                        from    'ionic-angular';
+import { Geolocation }                          from    '@ionic-native/geolocation';
 
-import { DarkSkyApiResponse }               from '../../models/darkskyapi-response.model';
-import { DarkSkyApiDataPoint }              from '../../models/darkskyapi-datapoint.model';
-import { DarkSkyApiDataBlock }              from '../../models/darkskyapi-datablock.model';
-import { IonicNativeGeoposition }           from '../../models/ionicnative-geoposition.model';
+import { DarkSkyApiResponse }                   from    '../../models/darkskyapi-response.model';
+import { DarkSkyApiDataPoint }                  from    '../../models/darkskyapi-datapoint.model';
+import { DarkSkyApiDataBlock }                  from    '../../models/darkskyapi-datablock.model';
+import { IonicNativeGeoposition }               from    '../../models/ionicnative-geoposition.model';
 
-import { DarkSkyApiService }                from '../../services/darkskyapi.service';
+import { DarkSkyApiService }                    from    '../../services/darkskyapi.service';
 
 @Component({
   selector: 'page-home',
@@ -15,14 +15,13 @@ import { DarkSkyApiService }                from '../../services/darkskyapi.serv
 })
 
 export class HomePage {
+
   //Object
   currentPosition: IonicNativeGeoposition = new IonicNativeGeoposition();
   currentForecast: DarkSkyApiDataPoint = new DarkSkyApiDataPoint();
-  dailyForcastFornextWeek: DarkSkyApiDataBlock = new DarkSkyApiDataBlock();
   hourlyForcastFornextTwoDays: DarkSkyApiDataBlock = new DarkSkyApiDataBlock();
-  minutlyForcastFornextHour: DarkSkyApiDataBlock = new DarkSkyApiDataBlock();
-
   response: DarkSkyApiResponse = new DarkSkyApiResponse();
+  forecastimage: string; //wille store the location of imae of forecast
 
   constructor(public navCtrl: NavController, private geolocation: Geolocation, private darkSkyApiService: DarkSkyApiService) {
     
@@ -31,6 +30,7 @@ export class HomePage {
     .then(fetched =>  {
       this.response = fetched;
       this.hydrate();
+      this.setIconToForecast(this.response.currently.icon);
     });    
   }
 
@@ -39,7 +39,6 @@ export class HomePage {
       console.log(this.response);
       this.currentForecast = this.response.currently;
       this.currentForecast.placeName = this.response.timezone;
-      this.dailyForcastFornextWeek = this.response.daily;
       this.hourlyForcastFornextTwoDays = this.response.hourly;
   }
 
@@ -62,4 +61,55 @@ export class HomePage {
 
   }
 
+  
+  private setIconToForecast(iconstring: string ) {
+    switch (iconstring) {
+      case "clear-day":
+         this.forecastimage = "assets/img/sunny.png";
+        break;
+      case "clear-night":
+        this.forecastimage = "";
+        break;
+      case "rain":
+        this.forecastimage = "assets/img/rainy.png";
+        break;
+      case "snow":
+        this.forecastimage = "assets/img/snow.png";
+        break;
+      case "sleet":
+        this.forecastimage = "assets/img/sleet.png";
+        break;
+      case "wind":
+        this.forecastimage = "";
+        break;
+      case "fog":
+        this.forecastimage = "";
+        break;
+      case "cloudy":
+        this.forecastimage = "assets/img/cloudy-day.png";
+        break;
+      case "partly-cloudy-day":
+        this.forecastimage = "assets/img/partly-sunny.png";
+        break;
+      case "partly-cloudy-night":
+        this.forecastimage = "assets/img/cloud.png";
+        break;
+      case "hail":
+        this.forecastimage = "";
+        break;
+      case "thunderstorm":
+        this.forecastimage = "assets/img/thunderstorm.png";
+        break;
+      case "tornado":
+        this.forecastimage = "";
+        break;
+      default:
+        break;
+    }
+  }
+
+  //Show another page
+  public showOtherLocations() {
+
+  }
 }
---
+++ src/pages/otherplaces/otherplaces.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-otherplaces',
+  templateUrl: 'otherplaces.html'
+})
+export class OtherPlacePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
