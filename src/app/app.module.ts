d99aee20a2bada6da13f62f8e66278973e31b98b
david95thinkcode <davidhihea@gmail.com>
2017-07-17 16:09:53 +0100

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,8 +17,8 @@ import { HttpModule  }                  from "@angular/http";
 import {  AngularFireModule }           from 'angularfire2';
 import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database'; 
 import { DarkSkyApiService }            from '../services/darkskyapi.service';
-import { FirebaseProvider } from '../providers/firebase/firebase';
-import { IonicNativeService} from '../services/ionicnative.service'
+import { FirebaseProvider }             from '../providers/firebase/firebase';
+import { IonicNativeService}            from '../services/ionicnative.service'
 
 //AngularFire2 Settings
 export const firebaseConfig = {
