e76ee98ebdeb66ff3387b782bb54ad38bda0b1e1
david95thinkcode <davidhihea@gmail.com>
2017-07-17 01:26:29 +0100

    List of all capitals available

--- src/pages/otherplaces/otherplaces.ts
+++ src/pages/otherplaces/otherplaces.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component }                    from '@angular/core';
+import { NavController }                from 'ionic-angular';
+import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'page-otherplaces',
   templateUrl: 'otherplaces.html'
 })
+
 export class OtherPlacePage {
 
-  constructor(public navCtrl: NavController) {
+ capitals: FirebaseListObservable<any>;   
 
+  constructor(public navCtrl: NavController, af: AngularFireDatabase) {
+    
+    //Loading capitals from firebase 
+    //And putting it to variable "capitals"
+    this.capitals = af.list('/capital');
+    
+    console.log(this.capitals);
+  }
+
+  /**SHOW FORECAST ABOUT LOCATION RECEIVED IN PARAMETERS */
+  showForecast(latitude: number, longitude: number) {
+    console.log("Location : " + latitude + " / " + longitude);
   }
 
 }
